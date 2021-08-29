488439593a99ca67d5c2559cf4fe4376bde18397
swillrich5 <swillrich@gmail.com>
2021-08-29 14:52:54 -0500

    added dotenv

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <h1 className="text-center">Marvel Application with Redux-Saga</h1>
       <h2 className="text-center">Hello World!</h2>
+      <h3>{process.env.REACT_APP_PUBLIC_KEY}</h3>
     </div>
   );
 }
