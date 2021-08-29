c2e5362a948f91a4b9473e378052a1e511f6acc7
swillrich5 <swillrich@gmail.com>
2021-08-29 14:23:32 -0500

    initial react setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Marvel Application with Redux-Saga</h1>
+      <h2>Hello World!</h2>
+    </div>
+  );
+}
+
+export default App;
